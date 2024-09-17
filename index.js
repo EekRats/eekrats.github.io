6130f2148296c2ada242a8d4741a1de44eaf21cc
EekRats <s.geacret@gmail.com>
2024-09-16 20:29:25 -0500

    Add content, collapsible button, styling

---
+++ index.js
@@ -0,0 +1,22 @@
+function pageLoad()
+{
+    var collapsibleItems = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < collapsibleItems.length; i++)
+    {
+      collapsibleItems[i].addEventListener("click", function()
+      {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight)
+        {
+          content.style.maxHeight = null;
+        }
+        else
+        {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
+}
